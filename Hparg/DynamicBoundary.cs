49d8ecc6179fa007d1163eed8ef7f0825ec33a66
Clement Carre <clement.carre@bionomeex.com>
2021-07-26 15:11:14 +0200

    Change DynamicLimit to DynamicBoundary

--- Hparg/DynamicBoundary.cs
+++ Hparg/DynamicBoundary.cs
@@ -1,8 +1,8 @@
 ﻿namespace Hparg
 {
-    internal class DynamicLimit
+    internal class DynamicBoundary
     {
-        public DynamicLimit(float value, bool isDynamic)
+        public DynamicBoundary(float value, bool isDynamic)
              => (Value, IsDynamic) = (value, isDynamic);
 
         public float Value { set; get; }
