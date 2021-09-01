5bb253daa28c668aaf71e670382d927c40b4aeb2
Xwilarg <xwilarg@yahoo.fr>
2021-09-01 02:49:35 +0200

    Change selection color

--- Hparg/Plot/APlot.cs
+++ Hparg/Plot/APlot.cs
@@ -121,7 +121,7 @@ namespace Hparg.Plot
                 var yMin = (int)Math.Min(_dragAndDropSelection.Value.start.Y, _dragAndDropSelection.Value.end.Y);
                 var xMax = (int)Math.Max(_dragAndDropSelection.Value.start.X, _dragAndDropSelection.Value.end.X);
                 var yMax = (int)Math.Max(_dragAndDropSelection.Value.start.Y, _dragAndDropSelection.Value.end.Y);
-                grf.DrawRectangle(new Pen(new SolidBrush(Color.Black)),
+                grf.DrawRectangle(new Pen(new SolidBrush(Color.Red)),
                     new Rectangle(xMin, yMin, xMax - xMin, yMax - yMin));
             }
 
