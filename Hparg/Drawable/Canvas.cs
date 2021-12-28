e7afa99f00b3ab134353239fbf1fbe51f0d5756b
Xwilarg <xwilarg@yahoo.fr>
2021-12-28 02:03:00 +0100

    Add offset in canvas

--- Hparg/Drawable/Canvas.cs
+++ Hparg/Drawable/Canvas.cs
@@ -9,8 +9,8 @@ namespace Hparg.Drawable
     {
         internal Canvas(int width, int height)
         {
-            _width = width;
-            _height = height;
+            _width = width - 2 * _offset;
+            _height = height - 2 * _offset;
             _bmp = new(width, height);
             _grf = Graphics.FromImage(_bmp);
             _grf.SmoothingMode = SmoothingMode.HighQuality;
@@ -23,11 +23,11 @@ namespace Hparg.Drawable
             switch (shape)
             {
                 case Shape.Circle:
-                    _grf.FillEllipse(brush, x * _width - size / 2, y * _height - size / 2, size, size);
+                    _grf.FillEllipse(brush, _offset + x * _width - size / 2, _offset + y * _height - size / 2, size, size);
                     break;
 
                 case Shape.Diamond:
-                    _grf.FillRectangle(brush, x * _width - size / 2, y * _height - size / 2, size, size);
+                    _grf.FillRectangle(brush, _offset + x * _width - size / 2, _offset + y * _height - size / 2, size, size);
                     break;
 
                 default:
@@ -38,13 +38,13 @@ namespace Hparg.Drawable
         public void DrawLine(float x1, float y1, float x2, float y2, int size, Color color)
         {
             _grf.DrawLine(new Pen(GetBrush(color), size),
-                new Point((int)(x1 * _width), (int)(y1 * _height)),
-                new Point((int)(x2 * _width), (int)(y2 * _height)));
+                new Point(_offset + (int)(x1 * _width), _offset + (int)(y1 * _height)),
+                new Point(_offset + (int)(x2 * _width), _offset + (int)(y2 * _height)));
         }
 
         public void DrawRectangle(float x, float y, float w, float h, int size, Color color)
         {
-            _grf.DrawRectangle(new(GetBrush(color), size), new((int)(x * _width), (int)(y * _height), (int)(w * _width), (int)(h * _height)));
+            _grf.DrawRectangle(new(GetBrush(color), size), new(_offset + (int)(x * _width), _offset + (int)(y * _height), (int)(w * _width), (int)(h * _height)));
         }
 
         /// <summary>
@@ -71,5 +71,7 @@ namespace Hparg.Drawable
         private readonly Graphics _grf;
 
         private readonly Dictionary<Color, Brush> brushes = new();
+
+        private const int _offset = 20;
     }
 }
